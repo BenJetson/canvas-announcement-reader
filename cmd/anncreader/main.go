bdb72216a9819c0ca7c85b7ae53ee4a33f6a93e5
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-09-03 21:14:18 -0400

    Option to source configuration interactively.

--- cmd/anncreader/main.go
+++ cmd/anncreader/main.go
@@ -4,14 +4,60 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/pkg/errors"
+
 	"github.com/BenJetson/canvas-announcement-reader/canvas"
 )
 
-func main() {
-	token := os.Getenv("CANVAS_TOKEN")
-	host := os.Getenv("CANVAS_HOST")
+type config struct {
+	token string
+	host  string
+}
 
-	api := canvas.NewAPI(host, token)
+func getConfigFromEnvironment() *config {
+	var cfg config
+	cfg.token = os.Getenv("CANVAS_TOKEN")
+	cfg.host = os.Getenv("CANVAS_HOST")
+
+	if len(cfg.token) < 1 || len(cfg.host) < 1 {
+		return nil
+	}
+	return &cfg
+}
+
+func getConfigInteractive() *config {
+	var cfg config
+
+	fmt.Println("Enter a the Canvas hostname for your school.")
+	fmt.Println(`For example, clemson.instructure.com for Clemson University.`)
+	fmt.Print(">>> ")
+	fmt.Scanln(&cfg.host)
+
+	if len(cfg.host) < 1 {
+		err := errors.New("Hostname cannot be blank.")
+		panic(err)
+	}
+
+	fmt.Println("Enter a Canvas access token for your account.")
+	fmt.Print(">>> ")
+	fmt.Scanln(&cfg.token)
+
+	if len(cfg.token) < 1 {
+		err := ("Access token cannot be blank.")
+		panic(err)
+	}
+
+	fmt.Println()
+	return &cfg
+}
+
+func main() {
+	cfg := getConfigFromEnvironment()
+	if cfg == nil {
+		cfg = getConfigInteractive()
+	}
+
+	api := canvas.NewAPI(cfg.host, cfg.token)
 
 	courses, err := api.GetActiveCourses()
 	if err != nil {
